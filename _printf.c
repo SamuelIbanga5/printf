ce37a356da5e4e5f8265f48701a5b89773c87ae6
Samuel Ibanga <ibangajnr27@gmail.com>
2022-10-16 14:29:48 +0100

    Started _printf source code

---
+++ _printf.c
@@ -0,0 +1,7 @@
+#include "main.h"
+/**
+ * printf - Function that produces output according to a format.
+ * @format: A character string.
+ * @...: Values to be substituted.
+ * Return: Number of characters printed.
+ */
