203aaa9dabaaf40a1c95365a86e31e63e157f789
Elizabeth_Nduka <ndukaelizabeth26@gmail.com>
2022-10-16 12:47:05 -0700

    Added Printf_s to printf_spec.c file

--- printf_spec.c
+++ printf_spec.c
@@ -1,12 +1,29 @@
 #include "main.h"
 /**
  * printf_c - A function that prints characters
- * @c: format of the arguments
+ * @c: list of the arguments
  *
- * Return: Amount of characters printed
+ * Return: characters printed
  */
 int printf_c(va_list c)
 {
 	_putchar(va_arg(c, int));
 	return (1);
 }
+
+/**
+ * printf_s - A function that prints a string
+ * @c: lists of the argument
+ *
+ * Return: characters printed
+ */
+int printf_s(va_list c)
+{
+	int i;
+	char *str = va_arg(c, char *);
+
+	for (i=0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	return (i);
+}
+
