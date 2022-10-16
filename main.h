1dd0031ade4463f60db11094ad966131594c6c8f
Samuel Ibanga <ibangajnr27@gmail.com>
2022-10-16 15:51:54 +0100

    Created structure for specifiers and their functions

--- main.h
+++ main.h
@@ -2,6 +2,16 @@
 #define _MAIN_H_
 
 #include <stdarg.h>
-#include <stdlib.h>
+
+/**
+ * struct print - Structure showing specifiers and connected functions.
+ * @spec: Specifier character.
+ * @spec_func: Specifiers function.
+ */
+struct print
+{
+	char *spec;
+	void (*func)(va_list);
+} print_specs;
 
 #endif /* _MAIN_H_ */
