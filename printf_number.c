e5be8191fbc1eeb9c30b2f04c4933c6c8ccaa2c6
Elizabeth_Nduka <ndukaelizabeth26@gmail.com>
2022-10-17 06:27:07 -0700

    added printf_number function

---
+++ printf_number.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * printf_d - print an integer in decimal form
+ * @n: the number to print
+ */
+int print_d(va_list d)
+{
+	int a[10];
+	int j, m, n, sum, count;
+
+	n = va_arg(d, int);
+	count = 0;
+	m = 1000000000;
+	a[0] = n / m;
+	for (j = 1; j < 10; j++)
+	{
+		m /= 10;
+		a[j] = (n / m) % 10;
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		count++;
+		for (j = 0; j < 10; j++)
+			a[j] *= -1;
+	}
+	for (j = 0, sum = 0; j < 10; j++)
+	{
+		sum += a[j];
+		if (sum != 0 || j == 9)
+		{
+			_putchar('0' + a[j]);
+			count++;
+		}
+	}
+	return (count);
+}
